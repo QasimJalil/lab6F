a84222d56b9e89aee90fdf41d9d60a140055416f
QasimJalil <qasimjalil2020@gmail.com>
2023-02-24 15:43:53 +0000

    Edit out syntax error

--- src/Exersice2.java
+++ src/Exersice2.java
@@ -2,23 +2,23 @@
 public class Exersice2 {
 
 
-		public static void main(String args[])
-		{
-			System.out.println(reverse("delzoobmab"));
-	}
-	
-	public static String reverse(String ipx)
+	public static void main(String args[])
 	{
-		StringBuilder reversed = new StringBuilder();
-		
-		for (int i = ipx.length()-1 ; i >= 0 ; i--)
-		{
-		reversed.append(ipx.charAt(i));
-		
-	//add your code here to reverse the string
-		return ipx;
-		}
+		//System.out.println(reverse("delzoobmab"));
 	}
+
+	//	public static String reverse(String ipx)
+	//	{
+	//		StringBuilder reversed = new StringBuilder();
+	//		
+	//		for (int i = ipx.length()-1 ; i >= 0 ; i--)
+	//		{
+	//		reversed.append(ipx.charAt(i));
+	//		
+	//	//add your code here to reverse the string
+	//		return ipx;
+	//		}
+	//	}
 }
 
 
